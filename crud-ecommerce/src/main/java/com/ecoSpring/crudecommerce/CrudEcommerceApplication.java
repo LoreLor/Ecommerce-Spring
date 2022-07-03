470ceaae4a009bb27f55ede5bab1c86eb05a9d54
LoreLor <lorenadearmas01@gmail.com>
2022-07-02 21:31:59 -0300

    configurando properties

--- crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/CrudEcommerceApplication.java
+++ crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/CrudEcommerceApplication.java
@@ -2,9 +2,9 @@ package com.ecoSpring.crudecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 public class CrudEcommerceApplication {
 
 	public static void main(String[] args) {
