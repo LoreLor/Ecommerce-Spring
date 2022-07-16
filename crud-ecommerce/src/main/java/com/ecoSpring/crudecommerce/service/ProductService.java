97462a6d11a6a03537b7b1b48bab286b45dbe80a
LoreLor <lorenadearmas01@gmail.com>
2022-07-16 19:19:40 -0300

    mostrar y agregar productos-funcional

--- crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/service/ProductService.java
+++ crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.ecoSpring.crudecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.ecoSpring.crudecommerce.model.Product;
 
 public interface ProductService {
+    public List<Product> findAll();
     public Product save(Product producto);
     public Optional<Product> get(Integer id);
     public void update(Product producto);
