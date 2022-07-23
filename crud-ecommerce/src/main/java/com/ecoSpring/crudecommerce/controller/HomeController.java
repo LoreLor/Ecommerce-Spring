ad1c7bf012051a0a65d06497fa2fa74dd6051f71
LoreLor <lorenadearmas01@gmail.com>
2022-07-23 17:11:04 -0300

    vista home usuario

--- crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/controller/HomeController.java
+++ crud-ecommerce/src/main/java/com/ecoSpring/crudecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.ecoSpring.crudecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecoSpring.crudecommerce.service.ProductService;
@@ -11,6 +14,8 @@ import com.ecoSpring.crudecommerce.service.ProductService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final Logger LOGGER= LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductService productService;
@@ -21,4 +26,12 @@ public class HomeController {
         return "usuario/home";
     }
     
+    @GetMapping("/producto/{id}")
+        public String productohome(@PathVariable Integer id){
+
+            LOGGER.info("id del producto enviado como parametro {}", id);
+
+            return "usuario/productohome";
+        
+    }
 }
